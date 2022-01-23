842b4fa8edd255f9a726b6c0d27ae495a85f1a7f
Dzmitry Iliukovich <mr.ilyukovich@gmail.com>
2022-01-23 15:37:30 +0300

    feat: map

--- rsclone-source/game/map/Ground_10.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Ground_10" tilewidth="32" tileheight="32" tilecount="16" columns="4">
- <image source="PNG/Platformer/Ground_10.png" width="128" height="128"/>
-</tileset>
--- rsclone-source/game/map/voody.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="voody" tilewidth="32" tileheight="32" tilecount="576" columns="18">
- <image source="../source-rsclone/rsclone-source/game/voody.png" width="576" height="1040"/>
-</tileset>
